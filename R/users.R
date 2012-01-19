e97f00f1b3b7feabec43b3c737c3db9f66f2ae60
Scott Chamberlain <myrmecocystus@gmail.com>
2012-01-18 18:19:46 -0600

    added two functions: users and genotypes

---
+++ R/users.R
@@ -0,0 +1,34 @@
+#' Get openSNP users.
+#' @import RJSONIO
+#' @param df Return data.frame (TRUE) or not (FALSE) - default = FALSE.
+#' @return List of openSNP users, their ID numbers, and XX if available.
+#' @export 
+#' @examples \dontrun{
+#' # just the list
+#' data <- users(df=FALSE)
+#' data 
+#' 
+#' # get a data.frame of the users data
+#' data <- users(df=TRUE)
+#' data[[1]] # users with links to genome data
+#' data[[2]] # users without links to genome data
+#' }
+users <- 
+
+function(df = FALSE, url = "http://opensnp.org/users.json") 
+{
+  users_ <- fromJSON(url)
+  if(df == FALSE){users_} else
+  {
+    lengths <- laply(users_, function(x) length(unlist(x)))
+    getdffive <- function(x) {
+      if(length(unlist(x)) == 5) data.frame(x)
+      }
+    getdftwo <- function(x) {
+      if(length(unlist(x)) == 2) data.frame(x[1:2])
+      }
+    withlinks <- ldply(users_, getdffive)
+    withoutlinks <- ldply(users_, getdftwo)
+    list(withlinks, withoutlinks)
+  }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# `ropensnp`
+
+You can get this package at CRAN here:
+https://github.com/ropensci/ropensnp
+
+Another option to install is install_github within Hadley's devtools package.
+
+```R
+install.packages("devtools")
+require(devtools)
+install_github("ropensnp", "rOpenSci")
+require(ropensnp)
+```
+
+This set of functions/package will access data from openSNP.org using their API methods. 
+
+See documentation on the openSNP API here:  http://opensnp.wordpress.com/2012/01/18/some-progress-on-the-api-json-endpoints/
