caecad1750089b26940bbf51417ba1bb72cb9449
Kevin Ushey <kevinushey@gmail.com>
2013-03-30 13:12:52 -0700

    adding NCBI_snp_query, LDSearch functions; merged from SNAPR

---
+++ R/NCBI_snp_query.R
@@ -0,0 +1,144 @@
+#' Query NCBI's dbSNP for information on a set of SNPs
+#' 
+#' This function queries NCBI's dbSNP for information related to the latest
+#' dbSNP build and latest reference genome for information on the vector
+#' of SNPs submitted.
+#' 
+#' @param SNPs A vector of SNPs (rs numbers).
+#' @export
+#' @return A dataframe with columns:
+#' \itemize{
+#' \item \code{Query:} The rs ID that was queried.
+#' \item \code{Chromosome:} The chromosome that the marker lies on.
+#' \item \code{Marker:} The name of the marker. If the rs ID queried
+#' has been merged, the up-to-date name of the marker is returned here, and
+#' a warning is issued.
+#' \item \code{Class:} The marker's 'class'. See
+#' \url{http://www.ncbi.nlm.nih.gov/projects/SNP/snp_legend.cgi?legend=snpClass}
+#' for more details.
+#' \item \code{Gene:} If the marker lies within a gene (either within the exon
+#' or introns of a gene), the name of that gene is returned here; otherwise,
+#' \code{NA}. Note that
+#' the gene may not be returned if the marker lies too far upstream or downstream
+#' of the particular gene of interest.
+#' \item \code{Alleles:} The alleles associated with the SNP if it is a
+#' SNV; otherwise, if it is an INDEL, microsatellite, or other kind of
+#' polymorphism the relevant information will be available here.
+#' \item \code{Major:} The major allele of the SNP, on the forward strand,
+#' given it is an SNV; otherwise, \code{NA}.
+#' \item \code{Minor:} The minor allele of the SNP, on the forward strand,
+#' given it is an SNV; otherwise, \code{NA}.
+#' \item \code{MAF:} The minor allele frequency of the SNP, given it is an SNV.
+#' This is drawn from the current global reference population used by NCBI.
+#' \item \code{BP:} The chromosomal position, in base pairs, of the marker, 
+#' as aligned with the current genome used by dbSNP.
+#' }
+#' @seealso \url{http://www.ncbi.nlm.nih.gov/projects/SNP/}
+#' @examples \dontrun{
+#' ## an example with both merged SNPs, non-SNV SNPs, regular SNPs, and
+#' ## SNPs not found
+#' SNPs <- c("rs332", "rs420358", "rs1837253", "rs1209415715")
+#' NCBI_snp_query(SNPs)
+#' }
+NCBI_snp_query <- function(SNPs) {
+  
+  query <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=snp&mode=xml&id="
+  query <- paste( sep='', query, paste( SNPs, collapse="," ) )
+  
+  xml <- getURL( query )
+  xml_parsed <- xmlInternalTreeParse( xml )
+  xml_list <- xmlToList( xml_parsed )
+  
+  ## Check which rs numbers were found, and warn if one was not found
+  found_snps <- unname( unlist( sapply( xml_list, function(x) {
+    ## check if the SNP is either in the current rsId, or the merged SNP list
+    possible_names <- c( tryget( x$.attrs["rsId"] ),
+                         tryget( x$MergeHistory )
+                       )
+    return(possible_names)
+  }) ) )
+  found_snps <- found_snps[ !is.na(found_snps) ]
+  found_snps <- paste( sep='', "rs", found_snps )
+  
+  if( any( !(SNPs %in% found_snps) ) ) {
+    warning("The following rsIds had no information available on NCBI:\n  ",
+            paste( SNPs[ !(SNPs %in% found_snps) ], collapse=", ")
+            )
+  }
+  SNPs <- SNPs[ SNPs %in% found_snps ]
+  ## The length of the xml list is equal to (num SNPs) + 1, as the last entry
+  ## is the schema location. So we iterate through items 1 to (n-1).'
+  
+  out <- NULL
+  
+  for( i in 1:(length(xml_list)-1) ) {
+    
+    my_list <- xml_list[[i]]
+    my_chr <- tryget(my_list$Assembly$Component$.attrs["chromosome"])
+    my_snp <- tryget( my_list$.attrs["rsId"] )
+    if( !is.na(my_snp) ) {
+      my_snp <- paste( sep='', "rs", my_snp )
+    }
+    if( my_snp != SNPs[i] ) {
+      warning( SNPs[i], " has been merged into ", my_snp )
+    }
+    my_snpClass <- tryget(my_list$.attrs["snpClass"])
+    
+    my_gene <- tryget( my_list$Assembly$Component$MapLoc$FxnSet['symbol'] )
+    alleles <- my_list$Ss$Sequence$Observed
+    
+    ## handle true SNPs
+    if( my_snpClass %in% c("snp", "snv") ) {
+      tmp <- c( my_list$Ss$Sequence$Observed, my_list$Ss$.attrs["orient"] )
+      if( tmp[2] != "forward" ) {
+        tmp[1] <- flip( tmp[1], sep="/" )
+      }
+      alleles_split <- unlist( strsplit( tmp[1], "/" ) )
+      
+      ## check which of the two alleles grabbed is actually the minor allele
+      ## we might have to 'flip' the minor allele if there is no match
+      maf_allele <- my_list$Frequency['allele']
+      if( all(maf_allele != alleles_split) ) {
+        maf_allele <- swap( maf_allele, c("A", "C", "G", "T"), c("T", "G", "C", "A") )
+      }
+      
+      my_minor <- alleles_split[ maf_allele == alleles_split ]
+      my_major <- alleles_split[ maf_allele != alleles_split ]
+      my_freq <- my_list$Frequency["freq"]      
+    } else { ## handle the others in a generic way; maybe specialize later
+      my_minor <- NA
+      my_major <- NA
+      my_freq <- NA
+    }
+    
+    my_pos <- tryCatch(
+      my_list$Assembly$Component$MapLoc$.attrs["physMapInt"],
+      error=function(e) { 
+        my_list$Assembly$Component$MapLoc["physMapInt"]
+      }
+    )
+    
+    dat <- data.frame(
+      Query = SNPs[i],
+      Chromosome = as.integer(my_chr),
+      Marker = my_snp,
+      Class = unname(my_snpClass),
+      Gene = unname(my_gene),
+      Alleles = unname(alleles),
+      Major = unname(my_major),
+      Minor = unname(my_minor),
+      MAF = as.numeric(my_freq),
+      BP = as.integer(my_pos),
+      stringsAsFactors=FALSE
+    )
+    
+    out <- rbind( out, dat )      
+    
+  }
+  
+  return(out)
+  
+  ## NCBI limits to a maximum of 1 query per three seconds; we
+  ## ensure that this limit is adhered to
+  Sys.sleep(3)
+}
--- tests/test-all.R
+++ tests/test-all.R
@@ -1,2 +1,2 @@
 library(testthat)
-test_package("ropensnp")
+test_package("rsnps")
