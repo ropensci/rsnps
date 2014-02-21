2dfecf8f73cdff183a41b8759934d8f8a5e99c29
Scott Chamberlain <myrmecocystus@gmail.com>
2014-02-21 06:52:28 -0800

    restyled man files due to new roxygen, fixed annotations fxn causing error in download before

--- R/annotations.R
+++ R/annotations.R
@@ -20,29 +20,33 @@
 #' @export 
 annotations <- function(snp = NA, output = c('all','plos','mendeley','snpedia','metadata')) 
 {
-	url = "http://opensnp.org/snps/json/annotation/"
-	message(paste(url, snp, '.json', sep=''))
-	out <- fromJSON(paste(url, snp, '.json', sep=''))
-	source_ <- match.arg(output, c('all','plos','mendeley','snpedia','metadata'), FALSE)
-	if(source_ == 'metadata'){
-		ldply(out$snp[c('name','chromosome','position')])
-	} else
-		if(source_ == 'all')
-			{
-				# replace NULL with "none" so that coercing to data.frame will work
-				ss <- function(x) { ifelse(is.null(x), "none", x) }
-				out <- llply(out$snp$annotations, function(x) llply(x, function(y) llply(y, ss)))
-			
-				ldply(out, function(x) ldply(x, function(s) as.data.frame(s)))
-			} else
-				{
-					out2 <- out$snp$annotations[[source_]] # Get part of list for given source_
-				
-					# replace NULL with "none" so that coercing to data.frame will work
-					ss <- function(x) { ifelse(is.null(x), "none", x) }
-					out2 <- llply(out2, function(x) llply(x, ss))
-				
-					# Get data.frame
-					ldply(out2, function(s) as.data.frame(s))
-				}
+  url = "http://opensnp.org/snps/json/annotation/"
+  url2 <- paste(url, snp, '.json', sep='')
+  message(url2)
+  res <- GET(url2)
+  stop_for_status(res)
+  out <- content(res)
+  source_ <- match.arg(output, c('all','plos','mendeley','snpedia','metadata'), FALSE)
+  
+  if(source_ == 'metadata'){
+    ldply(out$snp[c('name','chromosome','position')])
+  } else
+    if(source_ == 'all')
+    {
+      # replace NULL with "none" so that coercing to data.frame will work
+      ss <- function(x) { ifelse(is.null(x), "none", x) }
+      out <- llply(out$snp$annotations, function(x) llply(x, function(y) llply(y, ss)))
+      
+      ldply(out, function(x) ldply(x, data.frame, stringsAsFactors=FALSE))
+    } else
+    {
+      out2 <- out$snp$annotations[[source_]] # Get part of list for given source_
+      
+      # replace NULL with "none" so that coercing to data.frame will work
+      ss <- function(x) { ifelse(is.null(x), "none", x) }
+      out2 <- llply(out2, function(x) llply(x, ss))
+      
+      # Get data.frame
+      ldply(out2, data.frame, stringsAsFactors=FALSE)
+    }
 }
