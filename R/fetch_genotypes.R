0e62c509be88cfb9a9aaca0919585b891b0c0147
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-14 09:35:31 -0700

    fix to fetch_genotypes to be more general #22 bumped patch version

--- R/fetch_genotypes.R
+++ R/fetch_genotypes.R
@@ -1,23 +1,35 @@
-#' Download genotype data for a user from 23andme or other repo.
+#' Download genotype data for a user
 #'
-#' @param url URL for the download. See example below of function use.
-#' @param rows Number of rows to read in. Useful for getting a glimpse of the data.
-#' Default is 10 rows.
-#' @param filepath If none is given the file is saved to a temporary file, which will
-#' be lost after your session is closed. Save to a file if you want to access it later.
+#' @export
+#' @param url (character) URL for the download. See example below of function use.
+#' @param rows (integer) Number of rows to read in. Useful for getting a glimpse of 
+#' the data. Negative and other invalid values are ignored, giving back all data. 
+#' Default: 100
+#' @param filepath (character) If none is given the file is saved to a temporary file, 
+#' which will be lost after your session is closed. Save to a file if you want to 
+#' access it later.
+#' @param quiet (logical) Should download progress be suppressed. Default: \code{TRUE}
 #' @param ... Further args passed on to \code{\link{download.file}}
-#' @return Dataset for a single user.
+#' @return data.frame for a single user, with four columns:
+#' \itemize{
+#'  \item rsid (character)
+#'  \item chromsome (integer)
+#'  \item position (integer)
+#'  \item genotype (character)
+#' }
 #' @details Beware, not setting the rows parameter means that you download the entire
 #' file, which can be large (e.g., 15MB), and so take a while to download depending
 #' on your connection speed. Therefore, rows is set to 10 by default to sort of 
 #' protect the user. 
-#' @export 
+#' 
+#' Internally, we use \code{\link{download.file}} to download each file, then 
+#' \code{\link{read.table}} to read the file to a data.frame.
+#' 
 #' @examples \dontrun{
 #' # get a data.frame of the users data
-#' data <- users(df=TRUE)
+#' data <- users(df = TRUE)
 #' head( data[[1]] ) # users with links to genome data
-#' mydata <- fetch_genotypes(url = data[[1]][1,"genotypes.download_url"], 
-#'    file="~/myfile.txt", quiet=TRUE)
+#' mydata <- fetch_genotypes(url = data[[1]][1,"genotypes.download_url"], file="~/myfile.txt")
 #' 
 #' # see some data right away
 #' mydata
@@ -25,20 +37,10 @@
 #' # Or read in data later separately
 #' read.table("~/myfile.txt", nrows=10)
 #' }
-
-fetch_genotypes <- function(url, rows = 10, filepath = NULL, ...)
-{
-  #   if(is.null(rows)){
-  #     df <- read.table(as.character(url), skip=15, sep="\t", nrows = rows, header=FALSE)
-  #     names(df) <- c("rsid","chromsome","position","genotype")
-  #     df
-  #   } else
-  #   {
-  if(is.null(filepath)) 
-    filepath <- tempfile(fileext = ".txt")
-  download.file(url, method = "wget", destfile = filepath, ...)
-  df <- read.table(filepath, skip=15, sep="\t", nrows = rows, header=FALSE)
-  names(df) <- c("rsid","chromsome","position","genotype")
-  df
-  #   }
+fetch_genotypes <- function(url, rows = 100, filepath = NULL, quiet = TRUE, ...) {
+  if (is.null(filepath)) filepath <- tempfile(fileext = ".txt")
+  download.file(url, destfile = filepath, quiet = quiet, ...)
+  df <- read.table(filepath, sep = "\t", nrows = rows, header = FALSE, 
+                   comment.char = "#", stringsAsFactors = FALSE)
+  setNames(df, c("rsid", "chromsome", "position", "genotype"))
 }
