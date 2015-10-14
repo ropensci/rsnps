82fc148be70b13fac1a61faccb9638c7ad3a730a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-14 10:01:33 -0700

    added pkg level man file, importing non-base R fxns in utils pkg, fix #21

---
+++ R/rsnps-package.R
@@ -0,0 +1,12 @@
+#' Get SNP (Single-Nucleotide Polymorphism) Data on the Web
+#'
+#' This package gives you access to data from OpenSNP (http://opensnp.org/) via their API
+#' (https://opensnp.org/faq#api).
+#'
+#' @importFrom utils download.file read.table
+#' @name rsnps-package
+#' @aliases rsnps
+#' @docType package
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @author Kevin Ushey \email{kevinushey@@gmail.com}
+NULL
