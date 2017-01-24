3c01f7af69cd06ef2e499dc7b3c9928f5a98cd12
Vince Carey <stvjc@channing.harvard.edu>
2017-01-24 11:17:27 -0500

    minor changes proceeding towards check passage

---
+++ R/chain.R
@@ -0,0 +1,9 @@
+#' UCSC liftOver chain file mapping GRCh38 (hg38) to GRCh37 (hg19)
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Hg38toHg19.chain
+#' @usage data(Hg38toHg19.chain)
+#' @format An instance of \code{\link[rtracklayer]{Chain-class}}
+NULL
+
