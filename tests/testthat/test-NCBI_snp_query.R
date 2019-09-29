836550d69ec1ab6735db1d4f1fe7a75b32e3fde3
Sina Rueeger <sina.rueeger@gmail.com>
2019-09-29 20:27:15 +0300

    adding new test (merged variant)

--- tests/testthat/test-NCBI_snp_query.R
+++ tests/testthat/test-NCBI_snp_query.R
@@ -1,5 +1,28 @@
 context("ncbi_snp_query")
 
+test_that("ncbi_snp_query for rs1173690113 (merged into rs333)", {
+  ## truth: https://www.ncbi.nlm.nih.gov/snp/rs1173690113
+  
+  skip_on_cran()
+  
+  aa <- ncbi_snp_query("rs1173690113")
+  
+  
+  expect_equal(aa$Query, "rs1173690113")
+  expect_equal(aa$Chromosome, "3")
+  expect_equal(aa$BP, 46373453) ## on GRCh38 ##   chr3:46373453-46373487
+  expect_equal(aa$Marker, "rs333")
+  expect_equal(aa$Class, "indel")
+  expect_equal(aa$Gene, "CCR5,LOC102724297")
+  expect_equal(aa$Alleles, "GTCAGTATCAATTCTGGAAGAATTTCCAGACA")
+  expect_equal(aa$Major, "?")
+  expect_equal(aa$Minor, "?")
+  expect_equal(aa$MAF, 00)
+  expect_equal(aa$AncestralAllele, "?")
+
+})
+
+
 test_that("ncbi_snp_query for rs1421085", {
   ## truth: https://www.ncbi.nlm.nih.gov/snp/?term=rs1421085
   
