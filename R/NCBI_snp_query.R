cf1baed68925986b6eb2e3894a40db632396e4be
Sina Rueeger <sina.rueeger@gmail.com>
2019-09-29 19:54:49 +0300

    add new url

--- R/NCBI_snp_query.R
+++ R/NCBI_snp_query.R
@@ -80,7 +80,10 @@ ncbi_snp_query <- function(SNPs, key = NULL, ...) {
          "'rs', e.g. rs420358", call. = FALSE)
   }
 
-  url <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+  ## transform all SNPs into numbers (rsid)
+  SNPs_num <- gsub("rs", "", SNPs)
+  url <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
+  
   cli <- crul::HttpClient$new(url = url, opts = list(...))
   key <- check_key(key %||% "")
   res <- cli$get(query = rsnps_comp(list(db = 'snp', mode = 'xml', 
