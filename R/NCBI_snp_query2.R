cc7733f6ea5843a799772f89390f5ee10fe76627
Sina Rueeger <sina.rueeger@gmail.com>
2019-10-28 08:35:06 +0200

    fixing the missing documentation for ?ncbi_snp_query2

--- R/NCBI_snp_query2.R
+++ R/NCBI_snp_query2.R
@@ -1,6 +1,6 @@
 #' This function is defunct.
 #' @export
-#' @rdname ncbi_snp_query2-defunct
+#' @rdname ncbi_snp_query2-defunct-defunct
 #' @keywords internal
 ncbi_snp_query2 <- function(...) {
   .Defunct(new = "ncbi_snp_query", package = "rsnps", msg = "This function is defunct (NCBI API changed); use ncbi_snp_query() instead.")
