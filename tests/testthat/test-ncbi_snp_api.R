b5284e3188a144cc91df2d976a65e81ebcec7359
Julia Gustavsen <jgustavsen@gmail.com>
2020-08-14 21:49:09 +0200

    fix test related to lowercase snp

--- tests/testthat/test-ncbi_snp_api.R
+++ tests/testthat/test-ncbi_snp_api.R
@@ -209,7 +209,7 @@ test_that("ncbi_snp_query - gives warning when expected", {
 test_that("ncbi_snp_query fails well", {
   skip_on_cran()
 
-  expect_error(ncbi_snp_query(), "argument \"SNPs\" is missing")
+  expect_error(ncbi_snp_query(), "argument \"snps\" is missing")
   expect_error(ncbi_snp_query(5), "not all items supplied are prefixed")
   expect_error(ncbi_snp_query('ab5'), "not all items supplied are prefixed")
 
